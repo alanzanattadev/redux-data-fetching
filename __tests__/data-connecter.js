ffe2c631d81dd2f08c9a83a894eb1ae3d28e236b
alanzanattadev <alan.zanatta@outlook.com>
2016-10-30 17:29:02 +0100

    Adds new HOC component for graphql connection

--- __tests__/data-connecter.js
+++ __tests__/data-connecter.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Connecter from '../lib/graphql-data-connecter';
 import TestUtils from 'react-addons-test-utils';
 import {fromJS} from 'immutable';
+import GraphQLConnecter from "../lib/graphql-data-hoc";
+import {mount} from 'enzyme';
 
 describe('connecter', () => {
   it('should fetch data on mount and display', () => {
@@ -30,5 +32,60 @@ describe('connecter', () => {
     ));
     result = renderer.getRenderOutput();
     expect(result.props.name).toBe("Alan");
+  });
+});
+
+describe('HOC', () => {
+  it('should fetch data on mount', () => {
+    let MyComponent = ({name, description}) => (
+      <div>
+        <h1>{name}</h1>
+        <p>{description}</p>
+      </div>
+    );
+    let spy = jest.fn();
+    let MyFetcherComponent = GraphQLConnecter(props => `{user(id: "${props.id}") {name, description}}`, (cache, props) => cache.users[props.id])(MyComponent);
+    let subject = mount(<MyFetcherComponent dispatch={spy} data={{users: {'1': {name: 'Jon', description: 'Good'}}}} id={'1'}/>);
+
+    expect(spy).toBeCalledWith({
+      type: 'GRAPHQL_FETCH',
+      graphql: true,
+      payload: '{user(id: "1") {name, description}}'
+    });
+  });
+
+  it('should display with computed props', () => {
+    let MyComponent = ({name, description}) => (
+      <div>
+        <h1>{name}</h1>
+        <p>{description}</p>
+      </div>
+    );
+    let MyFetcherComponent = GraphQLConnecter(props => `{user(id: "${props.id}") {name, description}}`, (cache, props) => cache.users[props.id])(MyComponent);
+    let subject = mount(<MyFetcherComponent dispatch={() => {}} data={{users: {'1': {name: 'Jon', description: 'Good'}}}} id={'1'}/>);
+
+    expect(subject.find(MyComponent).at(0).prop('name')).toBe('Jon');
+    expect(subject.find(MyComponent).at(0).prop('description')).toBe('Good');
+  });
+
+  it('should refetch data when needs change', () => {
+    let MyComponent = ({name, description}) => (
+      <div>
+        <h1>{name}</h1>
+        <p>{description}</p>
+      </div>
+    );
+    let spy = jest.fn();
+    let data = {users: {'1': {name: 'Jon', description: 'Good'}, '2': {name: 'Doe', description: 'Bad'}}};
+    let MyFetcherComponent = GraphQLConnecter(props => `{user(id: "${props.id}") {name, description}}`, (cache, props) => cache.users[props.id])(MyComponent);
+    let subject = mount(<MyFetcherComponent dispatch={() => {}} data={data} id={'1'}/>);
+
+    subject.setProps({dispatch: spy, data: data, id: '2'});
+
+    expect(spy).toBeCalledWith({
+      type: 'GRAPHQL_FETCH',
+      graphql: true,
+      payload: '{user(id: "2") {name, description}}'
+    });
   });
 });
